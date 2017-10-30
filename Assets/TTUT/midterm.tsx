95350446e619fb52e37622975cbaa8beb90b9876
Scubafishy <32077848+Scubafishy@users.noreply.github.com>
2017-10-30 18:19:33 -0500

    testing

---
+++ Assets/TTUT/midterm.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="forest" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="../Sprites and Textures/ForestSheet.png" width="272" height="128"/>
+ <tile id="79">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Assets/TTUT/simmapbdaeiu.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="simmap2" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="../GES - Copy/GES 2D demo/Assets/sprites and textures/sheet.png" width="272" height="128"/>
+ <tile id="79">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
